7066ccb09ac779559057a094f285b87adfb80791
rickgroc <ricardo_guerrero@panterasup.com>
2026-01-16 11:17:15 -0600

    IntakeMotor RPMs
    Intake RPMs uploaded to smartdashboard and shown in elastic

--- SwerveWithPathPlanner/src/main/java/frc/robot/subsystems/Intake_demo.java
+++ SwerveWithPathPlanner/src/main/java/frc/robot/subsystems/Intake_demo.java
@@ -4,6 +4,7 @@
 
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.sim.TalonFXSimState;
@@ -14,16 +15,20 @@ import frc.robot.Constants;
 public class Intake_demo extends SubsystemBase {
  /** Creates a new Intake_demo. */
   private TalonFX IntakeMotor = new TalonFX(Constants.intake.motorID);
+
+  double IntakeMotorRPS = IntakeMotor.getVelocity().getValueAsDouble();
+  double IntakeMotorRPM = IntakeMotorRPS*60;
   public Intake_demo() {}
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+    SmartDashboard.putNumber("Intake RPMs", IntakeMotorRPM);
   }
 
   public void feed(double speed){
     IntakeMotor.set(speed);
-  }
+  } 
 
   public void stop(){
     IntakeMotor.set(0);
